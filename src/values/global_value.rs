0ee4a1edcb8cd8d75e7c1bcb4113aa89ef9dd7a7
hl <hl@gmail.col>
2023-12-12 15:53:29 +0400

    add build string to global_value

--- src/values/global_value.rs
+++ src/values/global_value.rs
@@ -290,6 +290,11 @@ impl<'ctx> GlobalValue<'ctx> {
     pub fn set_linkage(self, linkage: Linkage) {
         unsafe { LLVMSetLinkage(self.as_value_ref(), linkage.into()) }
     }
+    pub fn build_string(f: impl FnOnce(&RustString)) -> Result<String, FromUtf8Error> {
+        let sr = RustString { bytes: RefCell::new(Vec::new()) };
+        f(&sr);
+        String::from_utf8(sr.bytes.into_inner())
+    }
 }
 
 unsafe impl AsValueRef for GlobalValue<'_> {
