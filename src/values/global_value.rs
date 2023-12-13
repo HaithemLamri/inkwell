f1358cfa1eff98497d61869eb24ecfbbdac3b9eb
hl <hl@gmail.col>
2023-12-13 11:34:31 +0400

    fix import

--- src/values/global_value.rs
+++ src/values/global_value.rs
@@ -38,7 +38,7 @@ use crate::values::traits::AsValueRef;
 use crate::values::MetadataValue;
 use crate::values::{BasicValue, BasicValueEnum, PointerValue, Value};
 use crate::{DLLStorageClass, GlobalVisibility, ThreadLocalMode};
-use crate::types::RustString;
+use crate::types::*;
 use super::AnyValue;
 
 // REVIEW: GlobalValues are always PointerValues. With SubTypes, we should
